2874d159bccd392287312316a86d6bb4f6fe56a9
Jofre Vallès <jofrevalles99@gmail.com>
2023-05-07 19:45:12 +0200

    Fix  mutation type

--- src/Genetic.jl
+++ src/Genetic.jl
@@ -121,7 +121,7 @@ function mutate!(expr, mutation_probs, primitives_with_arity, parent::Union{Expr
 
     # Mutation type 7: duplicate a node within the expression (like mating an expression with itself)
     # TODO: Try to implement a custom node duplication for grad_dir
-    if rand() < mutation_probs[:duplicate_node] && (parent === nothing || !(parent.args[1] ∈ special_funcs && idx == 2))
+    if rand() < mutation_probs[:duplicate_node] && parent !== nothing && !(parent.args[1] ∈ special_funcs && idx == 2)
         target_pos = rand(2:length(parent.args))
         if idx != target_pos
             parent = update_parent_expr!(parent, target_pos, deepcopy(expr))
