8218da146e22cc6583559dc9417abd1297bc25e8
Jofre Vallès <jofrevalles99@gmail.com>
2024-04-25 20:15:48 +0200

    Update with new exports

--- src/GeneticTextures.jl
+++ src/GeneticTextures.jl
@@ -6,14 +6,17 @@ include("Color.jl")
 export Color, red, blue, green, ColorStyle
 
 include("PerlinBroadcasting.jl")
-include("CustomExpr.jl")
+include("GeneticExpr.jl")
+export GeneticExpr
+
 include("ExprGenerators.jl")
 export primitives_with_arity, random_expr
 
 include("MathOperations.jl")
+export gradient_functions
 
 include("ExprEvaluation.jl")
-export custom_eval
+export compile_expr
 
 include("Renderer.jl")
 export generate_image, save_image_and_expr
@@ -25,6 +28,6 @@ include("UI.jl")
 export generate_population, display_images, get_user_choice, create_variations
 
 include("DynamicalSystems.jl")
-export DynamicalSystem, VariableDynamics, animate_system, animate_system_2
+export DynamicalSystem, VariableDynamics, animate_system
 
 end
